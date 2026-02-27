690e1281d5169558938ecaa4afc76b972cc2028e
Chandresh-Kathiriya <kathiriyachandresh2@gmail.com>
2026-02-27 19:20:32 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ import App from './App';
 import store from './store';
 import './index.css';
 import './schemas/css/theme.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || '';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
